061db81fe9db20fcdf3276ba5e65d6f88fc670bf
chenjiefeng2001 <1497735430@qq.com>
2020-11-04 22:44:22 +0800

    2020.11.4

--- C++PrimerPlus/Chapter1/test3.cpp
+++ C++PrimerPlus/Chapter1/test3.cpp
@@ -1,4 +1,13 @@
 ﻿#include <iostream>
-int mian(){
-
+#include <math.h>
+int main()
+{
+    using namespace std;
+    double area;
+    cout << "enter the floor area, in square feet, of your home:";
+    cin >> area;
+    double side;
+    side = sqrt(area);
+    cout << "That's the equivalent of a square " << side << " feet to the side." << endl;
+    return 0;
 }
---
+++ C++PrimerPlus/Chapter1/test4.cpp
@@ -0,0 +1,15 @@
+﻿//ourfunc.cpp --defining your own function
+#include<iostream>
+void simon(int);
+int main(){
+    using namespace std;
+    simon(3);
+    cout<<"Pick an integer *";
+    int count;
+    cin>>count;
+    simon(count);
+    cout<<"Done!"<<endl;
+
+    return 0;
+
+}
