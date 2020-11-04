81a67f8ad9f950976d81aab7bd2fd5469f13cd59
chenjiefeng2001 <1497735430@qq.com>
2020-11-04 16:25:59 +0800

    pull

--- C++PrimerPlus/Chapter1/test2.cpp
+++ C++PrimerPlus/Chapter1/test2.cpp
@@ -1,5 +1,14 @@
 ﻿#include <iostream>
-int main(){
+int main()
+{
     using namespace std;
-
+    int carrots;//declare an integer variable
+    carrots = 25;
+    cout << "I have ";
+    cout << carrots; //display the value of the variable
+    cout << "carrots";
+    cout << endl;
+    carrots = carrots - 1; //modify the variable
+    cout<<"Curch,curch. Now I have "<<carrots<<" carrots "<<endl;
+    return 0;
 }
